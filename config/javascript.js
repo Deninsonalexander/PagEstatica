10dbbb517aae4476cce9bcf0e314be16657c3185
deninson <deninsonch.r7@gmail..com>
2022-11-12 16:11:35 -0500

    vv2

---
+++ config/javascript.js
@@ -0,0 +1,67 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }else{
+            RegistrarPersona();
+            event.preventDefault()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+function envio(){
+    let nombres = document.querySelector("#txtNombres").value;
+    let apellidos = document.querySelector("#txtApellidos").value;
+    let correo = document.querySelector("#txtCorreo").value;
+    let celular = document.querySelector("#txtCelular").value;
+    
+    let url = 'http://localhost:3000/envioNotificacion'
+    let datos = {
+        nombres: nombres,
+        apellidos: apellidos,
+        correo: correo,
+        celular: celular
+    }
+    fetch(url,{
+        method: 'POST',
+               
+    }).then(res => res.json()).then(mensaje =>{
+        console.log(mensaje)
+    })
+}
+
+function RegistrarPersona(){
+    let nombres = document.querySelector("#txtNombres").value;
+    let apellidos = document.querySelector("#txtApellidos").value;
+    let correo = document.querySelector("#txtCorreo").value;
+    let celular = document.querySelector("#txtCelular").value;
+    
+    let url = 'http://localhost:3000/personas'
+    let datos = {
+        nombres: nombres,
+        apellidos: apellidos,
+        correo: correo,
+        celular: celular
+    }
+    fetch(url,{
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers:{
+            'Content-Type':'application/json'
+        }        
+    }).then(res => res.json()).then(mensaje =>{
+        console.log(mensaje)
+    })
+}
